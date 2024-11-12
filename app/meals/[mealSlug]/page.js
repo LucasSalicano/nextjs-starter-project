b26d3c390a8d24f3039f0aa9d1582d1235738d74
lucassalicano <lucassalicano@hotmail.com>
2024-11-11 23:22:37 -0300

    initial commit

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,10 @@
+export default function MealDetailPage({ props }) {
+    return (
+        <>
+            <main>
+                <h1>Meals</h1>
+                <p>{props}</p>
+            </main>
+        </>
+    );
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,7 @@
+export default function Share() {
+    return (
+        <main>
+            <h1>Meals Share</h1>
+        </main>
+    );
+}
