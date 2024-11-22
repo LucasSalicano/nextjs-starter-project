9f8d5ceb4bddc2e25c8cc0f0b8e4b769e593659d
lucassalicano <lucassalicano@hotmail.com>
2024-11-21 22:30:38 -0300

    feat: add browse meals

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,26 @@
+import classes from './page.module.css';
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+
 export default function Meals() {
     return (
         <>
-            <main>
-                <h1>Meals</h1>
+            <header className={classes.header}>
+                <h1>
+                    Delicious Meals, created <span className={classes.highlight}> by you </span>
+                </h1>
+                <p>
+                    Choose your favorite meal from our broad selection of available meals and
+                    enjoy a delicious lunch or dinner at home.
+                </p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">
+                        Share your own meal!
+                    </Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <MealsGrid meals={[]} />
             </main>
         </>
     );
