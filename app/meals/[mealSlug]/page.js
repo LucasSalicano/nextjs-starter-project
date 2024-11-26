c9f183ab2739594e4faea9bb2336db250b05ae85
lucassalicano <lucassalicano@hotmail.com>
2024-11-26 20:20:35 -0300

    feat: add meal detail page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,35 @@
-export default function MealDetailPage({ props }) {
+import classes from './page.module.css';
+import Image from "next/image";
+import Link from "next/link";
+import {getMealBySlug} from "@/lib/meals";
+
+export default function MealDetailPage({params}) {
+    const meal = getMealBySlug(params.mealSlug);
+    meal.instructions = meal.instructions.replace(/\n/g, '<br>');
     return (
         <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill/>
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <Link href={`mailto:${meal.creator_email}`}>
+                        {meal.creator}
+                    </Link>
+                    </p>
+                    <p className={classes.summary}>
+                        {meal.summary}
+                    </p>
+                </div>
+            </header>
             <main>
-                <h1>Meals</h1>
-                <p>{props}</p>
+                <p
+                    className={classes.instructions}
+                    dangerouslySetInnerHTML={{
+                        __html: meal.instructions,
+                }}></p>
             </main>
         </>
     );
